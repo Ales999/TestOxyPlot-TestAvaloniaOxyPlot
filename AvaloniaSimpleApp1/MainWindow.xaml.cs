fa4e6b5f3e74085b7884eab69d102d9d305d59e5
Mekhanoshin Alexey <ales999@gmail.com>
2019-01-07 03:58:33 +0500

    Initialize commit

---
+++ AvaloniaSimpleApp1/MainWindow.xaml.cs
@@ -0,0 +1,23 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace AvaloniaSimpleApp1
+{
+    public class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+#if DEBUG
+            this.AttachDevTools();
+#endif
+            this.DataContext = new MainViewModel();
+        }
+
+        private void InitializeComponent()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+    }
+}
---
+++ AvaloniaSimpleApp1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Avalonia;
+using Avalonia.Logging.Serilog;
+using OxyPlot.Avalonia;
+using Serilog;
+
+
+namespace AvaloniaSimpleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            OxyPlotModule.EnsureLoaded();
+            InitializeLogging();
+            BuildAvaloniaApp().Start<MainWindow>();
+        }
+
+        private static void InitializeLogging()
+        {
+#if DEBUG
+            SerilogLogger.Initialize(new LoggerConfiguration()
+                .MinimumLevel.Warning()
+                .WriteTo.Trace(outputTemplate: "{Area}: {Message}")
+                .CreateLogger());
+#endif
+
+        }
+
+        public static AppBuilder BuildAvaloniaApp()
+            => AppBuilder.Configure<App>()
+                .UsePlatformDetect()
+                .BeforeStarting(_ => OxyPlotModule.Initialize())
+                .LogToDebug();
+    }
+}
